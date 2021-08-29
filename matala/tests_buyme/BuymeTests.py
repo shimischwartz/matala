bbac2b1e5216ce1986b9aefec61c47279082a554
shimischwartz <shimon.schwartz@mail.ac.il>
2021-08-30 02:34:30 +0300

    very late at night

--- matala/tests_buyme/BuymeTests.py
+++ matala/tests_buyme/BuymeTests.py
@@ -9,40 +9,46 @@ from tests_buyme.creatConfig import CreateConfig
 RANDOM_NUM = randint(1, 10000)
 INPUT = {"browser_type": "chrome",
          "url": "https://buyme.co.il",
-         "excepted_title": "title_to_insert",
+         "excepted_title": "BUYME אתר המתנות והחוויות הגדול בישראל | Gift Card",
          "first_name": "Shimon",
          'mail': f"S1.h{RANDOM_NUM}@gmail.com",
          "password": f"S1.h{RANDOM_NUM}@gmail.com",
-         "password_confirm": f"S1.h{RANDOM_NUM}@gmail.com"}
+         "password_confirm": f"S1.h{RANDOM_NUM}@gmail.com",
+         "subscribed_name": "Shimon",
+         "subscribed_email": f"S1.h{RANDOM_NUM}@gmail.com"}
 
 
 class BuymeTests(unittest.TestCase):
 
+    def __init__(self):
+        super().__init__()
+        self.tree = etree.parse("matala/buymeObjects/config_buy_me_ex").root()
+
     def test_something(self):
         self.assertEqual(True, False)
 
-    def creat_configuration_file(self):
+    def setUpClass(cls):
+        cls.creat_configuration_file()
+
+    @classmethod
+    def creat_configuration_file(cls):
         config = CreateConfig("buy_me_website")
         for key, value in INPUT.items():
             config.add_child(key, INPUT[key])
         config.write_to_file("config_buy_me_ex")
 
-    def raed_config(self):
-        self.tree = etree.parse("matala/buymeObjects/config_buy_me_ex").root()
-        self.browser_type = self.tree.find("browser_type").text
-        self.expected_title = self.tree.find("expected_title").text
-        firs_name = self.tree.find("first_name").text
-        password = self.tree.find("password").text
-        password_confirm = self.tree.find("password_confirm").text
-        c = BuyMeObjects("chrome")
-        c.set_first_name("Shimon")
-
-        c.set_mail_element()
-        c.set_password()
-        c.set_password_verification(f"S1.h{new_num}@gmail.com")
+    def test_subsection(self):
+        c = BuyMeObjects(self.tree.find("browser_type").text)
+        c.set_first_name(self.tree.find("first_name").text)
+        c.set_mail_element(self.tree.find("email").text)
+        c.set_password(self.tree.find("password").text)
+        c.set_password_verification(self.tree.find("password_confirm").text)
         c.set_subscribe_element()
+
+    def raed_config(self):
+        self.expected_title = self.tree.find("expected_title").text
         print(c.get_page_title())
-        print(c.check_subscription("Shimon", f"S1.h{new_num}@gmail.com"))
+        print(c.check_subscription(self.tree.find("subscribed_name").text, self.tree.find("subscribed_email").text))
 
 
 if __name__ == '__main__':
